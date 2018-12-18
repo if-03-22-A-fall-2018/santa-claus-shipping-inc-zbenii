55e6df6bd28fc509515e5b75bc05d324735d7ae9
zbenii <besic.beni@gmx.at>
2018-12-18 11:17:00 +0100

    list done

--- list.cpp
+++ list.cpp
@@ -3,12 +3,11 @@
 
 struct _node {
   void* data;
-  Node* next;
+  struct _node* next;
 };
 
 struct _list{
   Node* _head;
-  Node* _tail;
 };
 
 
@@ -18,25 +17,41 @@ List* list_create(){
 }
 
 void list_delete(List *list){
-  free(*list);
+  free(list);
 }
 
 void list_add(List *list, void *data){
 
+  Node* node=(Node*)malloc(sizeof(struct _node));
+  node->data=data;
+
+  if(list->_head==0){list->_head=node;}
+  else
+  {
+    node->next=list->_head;
+    list->_head=node;
+  }
+
 }
 
 void list_insert_after(Node *node, void *data){
+  Node* newNode=(Node*)malloc(sizeof(struct _node));
+  newNode->data=data;
+
+  newNode->next=node->next;
+  node->next=newNode;
+
 
 }
 
 Node* list_get_first(List *list){
-
+return list->_head;
 }
 
 Node* list_get_next(Node *node){
-
+return node->next;
 }
 
 void* list_get_data(Node *node){
-
+ return node->data;
 }
